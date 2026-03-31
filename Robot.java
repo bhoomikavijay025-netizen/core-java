2e7feb907f7b6e61fb8fcc25dfa6c07efc921a17
bhoomikavijay025-netizen <bhoomikavijay025@gmail.com>
2026-03-31 14:31:25 +0530

    day 30

---
+++ Robot.java
@@ -0,0 +1,18 @@
+class Robot {
+    int robotId;
+    String robotName;
+    double batteryCapacity;
+    float speed;
+    boolean isAutonomous;
+    char version;
+    long serialNumber;
+	
+	public Robot(){
+		System.out.println("default construct");
+		
+	}
+}
+
+ 
+
+      
---
+++ Table.java
@@ -0,0 +1,15 @@
+class Table {
+    String brandName;
+    String material;
+    float height;
+    double price;
+    char size;
+    int tableId;
+    boolean isAvailable;
+	
+	public Table(){
+		
+   System.out.println("default const");
+	}
+}
+
