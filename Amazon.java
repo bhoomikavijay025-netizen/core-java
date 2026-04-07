448d53091a1014a1ea4f03635c283b6b42d328ef
bhoomikavijay025-netizen <bhoomikavijay025@gmail.com>
2026-04-07 17:01:36 +0530

    constructor initial

--- Amazon.java
+++ Amazon.java
@@ -1,78 +1,76 @@
 class Amazon {
 
-   
-    static int checkOrder(String product) {
+    static int checkProduct(String item) {
 
         int price = 0;
 
-        if ("laptop".equals(product)) {
-            System.out.println("product is: " + product);
+        if (item == "laptop") {
+            System.out.println("item is: " + item);
             price = 50000;
+            return price;
 
-        } else if ("mobile".equals(product)) {
-            System.out.println("product is: " + product);
-            price = 25000;
+        } else if (item == "mobile") {
+            System.out.println("item is: " + item);
+            price = 20000;
+            return price;
 
-        } else if ("headphone".equals(product)) {
-            System.out.println("product is: " + product);
-            price = 5000;
-
-        } else if ("earphone".equals(product)) {
-            System.out.println("product is: " + product);
-            price = 250;
-
-        } else if ("charger".equals(product)) {
-            System.out.println("product is: " + product);
-            price = 300;
-
-        } else if ("bluetooth".equals(product)) {
-            System.out.println("product is: " + product);
-            price = 200;
-
-        } else if ("tv".equals(product)) {
-            System.out.println("product is: " + product);
-            price = 10000;
-
-        } else if ("computers".equals(product)) {
-            System.out.println("product is: " + product);
-            price = 27000;
-
-        } else if ("camera".equals(product)) {
-            System.out.println("product is: " + product);
+        } else if (item == "tablet") {
+            System.out.println("item is: " + item);
             price = 15000;
+            return price;
 
-        } else if ("smart home".equals(product)) {
-            System.out.println("product is: " + product);
-            price = 25300;
+        } else if (item == "headphone") {
+            System.out.println("item is: " + item);
+            price = 3000;
+            return price;
 
-        } else if ("speaker".equals(product)) {
-            System.out.println("product is: " + product);
-            price = 16000;
+        } else if (item == "earphone") {
+            System.out.println("item is: " + item);
+            price = 500;
+            return price;
 
-        } else if ("battery".equals(product)) {
-            System.out.println("product is: " + product);
+        } else if (item == "charger") {
+            System.out.println("item is: " + item);
+            price = 700;
+            return price;
+
+        } else if (item == "powerbank") {
+            System.out.println("item is: " + item);
             price = 1200;
+            return price;
+
+        } else if (item == "camera") {
+            System.out.println("item is: " + item);
+            price = 25000;
+            return price;
+
+        } else if (item == "smartwatch") {
+            System.out.println("item is: " + item);
+            price = 5000;
+            return price;
+
+        } else if (item == "speaker") {
+            System.out.println("item is: " + item);
+            price = 4000;
+            return price;
 
         } else {
             System.out.println("no item found");
             return 0;
         }
-
-        return price; 
     }
 
+    static int calculateTotal(String item, int price, int quantity) {
 
-    static int checkItem(String product, int price, int noItems) {
+        int total = 0;
 
-        int totalPrice = 0;
+        if (item == "laptop") {
+            total = price * quantity;
+            return total;
 
-        if ("laptop".equals(product)) {
-            totalPrice = price * noItems;
-            return totalPrice;
-
-        } else if ("bluetooth".equals(product)) {
-            totalPrice = price * noItems;
-            return totalPrice;
+        } else if (item == "mobile") {
+            total = price * quantity;
+            return total;
 
         } else {
             return 0;
